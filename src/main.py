567b4442e5e8e255e928f7de8acb16723089a58f
Willie Chalmes III <willieechalmers@gmail.com>
2019-04-03 09:45:31 -0600

    chore: Update threading and scaffold I2C (for real)

--- src/main.py
+++ src/main.py
@@ -1,18 +1,58 @@
-#!/usr/bin/env sh
+#!/usr/bin/env python3
+
+from smbus import SMBus
+from threading import Thread
+import time
+
+
+I2C_SLAVE_ADDRESS = 0x8
+
+bus = SMBus(1)
+
+
+class SensorSnapshot:
+    """A container for sensor values obtained at a given moment."""
+    def __init__(self, x: float, y: float, z: float, flex: float):
+    self.x = x
+    self.y = y
+    self.z = z
+    self.flex = flex
+
+
+def send_number(value):
+    bus.write_byte(I2C_SLAVE_ADDRESS, value)
+    
+    return -1
+
+
+def read_value():
+    number = bus.read_byte(I2C_SLAVE_ADDRESS)
+    return number
+
+
+def on_i2c_receive():
+
+    print(
+    pass
 
 
 def monitor_augment_controllers():
-    while (True):
+    while True:
         pass
 
 
 def watch_assistant():
-    pass
+    while True:
+        pass
 
 
 def main():
-    monitor_augment_controllers()
-    watch_assistant()
+    augment_monitor_thread = Thread(target=monitor_augment_controllers, args=[])
+    assistant_thread = Thread(target=watch_assistant, args=[])
+    augment_monitor_thread.start()
+    augment_monitor_thread.join()
+    assistant_thread.start()
+    assistant_thread.join()
 
 
 if __name__ is '__main__':
