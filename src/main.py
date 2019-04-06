065e28dc53592ea669f49b39248824650ae46187
Willie Chalmers III <willieechalmers@gmail.com>
2019-04-05 18:13:31 -0600

    chore: Set up foundations for Hub functions
    This commit adds two classes, the AugmentControllerMonitor and the
    AssistantCommandMonitor, that watch for Augment Controller signals
    and Assistant queries, respectively.

--- src/main.py
+++ src/main.py
@@ -1,59 +1,121 @@
 #!/usr/bin/env python3
+"""The primary entrypoint into monitoring Digibod components."""
 
-from smbus import SMBus
-from threading import Thread
 import time
 
-
-I2C_SLAVE_ADDRESS = 0x8
-
-bus = SMBus(1)
+from threading import Thread
+from smbus import SMBus
+from train import DigibodTrainer
 
 
 class SensorSnapshot:
-    """A container for sensor values obtained at a given moment."""
+    """A container for sensor values obtained at a given moment.
+
+    Attributes:
+        x (float): The horizontal axis movement
+        y (float): The vertical axis movement
+        z (float): The deptch axis movement
+    """
+
     def __init__(self, x: float, y: float, z: float, flex: float):
-    self.x = x
-    self.y = y
-    self.z = z
-    self.flex = flex
+        self.x = x
+        self.y = y
+        self.z = z
+        self.flex = flex
 
 
-def send_number(value):
-    bus.write_byte(I2C_SLAVE_ADDRESS, value)
-    
-    return -1
+class AugmentControllerMonitor(Thread):
+    """A utility that interacts with Digibod Augment Controllers.
+
+    This monitor observes and sends data to and from one or more Digibod
+    Augment Controllers over I2C.
+    """
+
+    CONTROLLER_I2C_ADDRESS = 0x16
+
+    def __init__(self):
+        super().__init__()
+        self._should_stop = False
+        self._bus = SMBus(1)
+
+    def start_monitoring(self):
+        """Begin listening to assistant queries"""
+        self.start()
+        self.join()
+
+    def run(self):
+        while not self._should_stop:
+            pass
+
+    def stop_monitoring(self):
+        """Allow this thread to stop"""
+        self._should_stop = True
 
 
-def read_value():
-    number = bus.read_byte(I2C_SLAVE_ADDRESS)
-    return number
+class AssistantCommandMonitor(Thread):
+    """A thread that monitors a microphone for Google Assistant queries
 
+    TODO: Set up Google Assistant
+    """
 
-def on_i2c_receive():
+    def __init__(self):
+        super().__init__()
+        self._should_stop = False
 
-    print(
-    pass
+    def start_monitoring(self):
+        """Begin listening to assistant queries"""
+        self.start()
+        self.join()
 
+    def run(self):
+        while not self._should_stop:
+            pass
 
-def monitor_augment_controllers():
-    while True:
-        pass
+    def stop_monitoring(self):
+        """Allow this thread to stop"""
+        self._should_stop = True
 
+class CommandInterpreter:
+    """A wrapper for data collection and inference functions.
 
-def watch_assistant():
-    while True:
-        pass
+    Attributes:
+        is_training (bool): True if the program is waiting for training
+                            to finish
+    """
+
+    def __init__(self):
+        self.is_training = False
+        self._trainer = DigibodTrainer()
+
+    def collect_data(self, millis: bool = 2000):
+        """Temporarily begin collecting sensor snapshots."""
+
+    def train(self):
+        """Synchronously begin training a new model"""
+        self.is_training = True
+        # TODO: Begin training with trainer
+        self.is_training = False
+
+    def infer(self, snapshot_length: int = 100):
+        """Briefly collect sensor values and determine taken action
+
+        Args:
+            snapshot_length (int): The amount of milliseconds
+                                   used to collect data to infer the action.
+        """
 
 
 def main():
-    augment_monitor_thread = Thread(target=monitor_augment_controllers, args=[])
-    assistant_thread = Thread(target=watch_assistant, args=[])
-    augment_monitor_thread.start()
-    augment_monitor_thread.join()
-    assistant_thread.start()
-    assistant_thread.join()
+    """Start all Digibod Augment Hub primary functions.
+
+    This function starts Augment Controller data monitoring as well as
+     Assistant query monitoring.
+    """
+    augment_monitor = AugmentControllerMonitor()
+    assistant_monitor = AssistantCommandMonitor()
+    augment_monitor.start_monitoring()
+    assistant_monitor.start_monitoring()
 
 
-if __name__ is '__main__':
+if __name__ == '__main__':
     main()
